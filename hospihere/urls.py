0a69792ea9f895bcdbd5a46f23e6f8f29f4b8867
sohan <abu.sohan@northsouth.edu>
2020-11-24 03:53:41 +0600

    vue js and bloodBand app added

--- hospihere/urls.py
+++ hospihere/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('first_login',TemplateView.as_view(template_name="login.html")),
     path('',include('sign_in.urls')),
     path('',include('pre_book.urls')),
+    path('',include('bloodBank.urls')),
     path('admin/', admin.site.urls),
     path('', include('hospital_list.urls')),
 ]
