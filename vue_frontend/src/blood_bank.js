46bb91d632e4bbc358e2773e7ad1e383e27ad1fc
sohan <abu.sohan@northsouth.edu>
2020-11-28 01:31:47 +0600

    pagination and row items bug fixed of bloodBank page

--- vue_frontend/src/blood_bank.js
+++ vue_frontend/src/blood_bank.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import blood_bank from './blood_bank.vue'
 import vuetify from './plugins/vuetify';
 
+import 'vuetify/dist/vuetify.min.css'
+
+
+
 Vue.config.productionTip = false
 
 new Vue({
--- vue_frontend/src/blood_bank.vue
+++ vue_frontend/src/blood_bank.vue
@@ -1,4 +1,5 @@
 <template>
+  <v-app>
   <v-card>
     <v-card-title>
       Blood Bank
@@ -8,15 +9,27 @@
         append-icon="mdi-magnify"
         label="Search Blood"
         single-line
-        hide-details
+        
       ></v-text-field>
     </v-card-title>
     <v-data-table
       :headers="headers"
       :items="donors"
       :search="search"
+      class="elevation-1"
+      
+      :footer-props="{
+      showFirstLastPage: true,
+      disableItemsPerPage: false,
+      disablePagination: false,
+      firstIcon: 'mdi-arrow-collapse-left',
+      lastIcon: 'mdi-arrow-collapse-right',
+      prevIcon: 'mdi-minus',
+      nextIcon: 'mdi-plus'
+    }"
     ></v-data-table>
   </v-card>
+  </v-app>
 </template>
 
 <script>
