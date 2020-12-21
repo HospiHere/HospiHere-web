6667f5ba74ea2cd8b8aff1982334c799498caf30
sohan <abu.sohan@northsouth.edu>
2020-12-22 00:23:46 +0600

    hospiProfile updated without changing pass and specialization

--- hospiProfile/urls.py
+++ hospiProfile/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('hospiProfile',views.hospiProfile,name='hospiProfile'),
+    path('branch',views.branch,name='branch'),
+    path('email',views.email,name='email'),
+    path('bed',views.bed,name='bed'),
 ]
--- hospiProfile/views.py
+++ hospiProfile/views.py
@@ -11,5 +11,61 @@ from google.cloud.firestore_v1 import Increment
 db = firestore.client()
 
 def hospiProfile(request):
+    user = request.user.username
+    doc_ref = db.collection(u'hospitals').where(u'name', u'==', user).stream()
+    items=[]
+    for doc in doc_ref:
+        items.append(doc.to_dict())
         
-    return render(request,"hospiProfile.html",)
+    return render(request,"hospiProfile.html", {'content': items})
+
+def branch(request):
+    user = request.user.username
+    branch = request.POST.get('branch')
+
+    db.collection(u'hospitals').document(user).update({
+        "branch" : branch,
+        })
+
+    doc_ref = db.collection(u'hospitals').where(u'name', u'==', user).stream()
+    items=[]
+    for doc in doc_ref:
+        items.append(doc.to_dict())
+        
+    return render(request,"hospiProfile.html", {'content': items})
+
+def email(request):
+    user = request.user.username
+    email = request.POST.get('email')
+
+    db.collection(u'hospitals').document(user).update({
+        "email" : email,
+        })
+
+    doc_ref = db.collection(u'hospitals').where(u'name', u'==', user).stream()
+    items=[]
+    for doc in doc_ref:
+        items.append(doc.to_dict())
+        
+    return render(request,"hospiProfile.html", {'content': items})
+
+def bed(request):
+    user = request.user.username
+    icu = request.POST.get('icu')
+    emergency = request.POST.get('emergency')
+    ward = request.POST.get('ward')
+
+    db.collection(u'hospitals').document(user).update({
+        "bed" : {
+                        "emergency" : int(emergency),
+                        "icu" : int(icu),
+                        "ward": int(ward),
+                    },
+        })
+
+    doc_ref = db.collection(u'hospitals').where(u'name', u'==', user).stream()
+    items=[]
+    for doc in doc_ref:
+        items.append(doc.to_dict())
+        
+    return render(request,"hospiProfile.html", {'content': items})
