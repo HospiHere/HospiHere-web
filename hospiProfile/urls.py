fd9ce72c326f7af5567caa9ebff358ba8f6a3136
sohan <abu.sohan@northsouth.edu>
2020-12-23 11:48:04 +0600

    hospiProfile updated

--- hospiProfile/urls.py
+++ hospiProfile/urls.py
@@ -5,5 +5,10 @@ urlpatterns = [
     path('hospiProfile',views.hospiProfile,name='hospiProfile'),
     path('branch',views.branch,name='branch'),
     path('email',views.email,name='email'),
+    path('latitude',views.latitude,name='latitude'),
     path('bed',views.bed,name='bed'),
+    path('test',views.test,name='test'),
+    path('remove(?P<test>\w+)$',views.remove,name='remove'),
+    path('specialized',views.specialized,name='specialized'),
+    path('removeSp(?P<specialized>\w+)$',views.removeSp,name='removeSp'),
 ]
--- hospiProfile/views.py
+++ hospiProfile/views.py
@@ -49,6 +49,23 @@ def email(request):
         
     return render(request,"hospiProfile.html", {'content': items})
 
+def latitude(request):
+    user = request.user.username
+    latitude = request.POST.get('latitude')
+
+    #db.collection(u'hospitals').document(user).update({
+    #    "location" : {
+    #        "latitude" : latitude,
+    #    }
+    #    })
+
+    doc_ref = db.collection(u'hospitals').where(u'name', u'==', user).stream()
+    items=[]
+    for doc in doc_ref:
+        items.append(doc.to_dict())
+        
+    return render(request,"hospiProfile.html", {'content': items})
+
 def bed(request):
     user = request.user.username
     icu = request.POST.get('icu')
@@ -62,6 +79,60 @@ def bed(request):
                         "ward": int(ward),
                     },
         })
+
+    doc_ref = db.collection(u'hospitals').where(u'name', u'==', user).stream()
+    items=[]
+    for doc in doc_ref:
+        items.append(doc.to_dict())
+        
+    return render(request,"hospiProfile.html", {'content': items})
+
+def test(request):
+    user = request.user.username
+    test = request.POST.getlist('DynamicTextBox')
+
+    test_ref = db.collection(u'hospitals').document(user)
+    test_ref.update({u'availableTest': firestore.ArrayUnion(test)})
+
+    doc_ref = db.collection(u'hospitals').where(u'name', u'==', user).stream()
+    items=[]
+    for doc in doc_ref:
+        items.append(doc.to_dict())
+        
+    return render(request,"hospiProfile.html", {'content': items})
+
+def remove(request, test):
+    user = request.user.username
+
+    test_ref = db.collection(u'hospitals').document(user)
+    test_ref.update({u'availableTest': firestore.ArrayRemove([test])})
+
+    doc_ref = db.collection(u'hospitals').where(u'name', u'==', user).stream()
+    items=[]
+    for doc in doc_ref:
+        items.append(doc.to_dict())
+        
+    return render(request,"hospiProfile.html", {'content': items})
+
+def specialized(request):
+    user = request.user.username
+    diseaseSpecialized = request.POST.getlist('DynamicSpecialistBox')
+
+    specialization = db.collection(u'hospitals').document(user)
+    specialization.update({u'specialization': firestore.ArrayUnion(diseaseSpecialized)})
+
+    doc_ref = db.collection(u'hospitals').where(u'name', u'==', user).stream()
+    items=[]
+    for doc in doc_ref:
+        items.append(doc.to_dict())
+        
+    return render(request,"hospiProfile.html", {'content': items})
+
+def removeSp(request, specialized):
+    user = request.user.username
+
+    specialization = db.collection(u'hospitals').document(user)
+    specialization.update({u'specialization': firestore.ArrayRemove([specialized])})
 
     doc_ref = db.collection(u'hospitals').where(u'name', u'==', user).stream()
     items=[]
